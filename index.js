2c5a0eeb8a9b77a297398ab4e3015fdb2aeef090
Kamran <mdkamran0109@gmail.com>
2024-03-11 00:07:05 +0530

    menu_api

--- index.js
+++ index.js
@@ -26,7 +26,26 @@ app.get("/restaurant-cards", async (req, res) => {
   }
 });
 
+app.get("/restaurants/:resId",async( req,res) =>{
+  try {
+    const userAgent ="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0";
+    let {data} = await axios(`${process.env.MENU_API}/${req.params.resId}`,{
+      headers:{
+        "User-Agent":userAgent,
+      },
+    });
+
+return res.json(data);
+  } catch (error) {
+    return res.json({ error: error });
+  }
+})
+
+
+
 const PORT = 3005 | process.env.PORT;
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
 });
+
+// 588705
